0aac630a0c0b61651304723cd420e16285464a79
Harry <harry@Harrys-MacBook-Pro.local>
2021-10-09 23:00:49 +0200

    Initial import.

---
+++ luzie-locke-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  luzie-locke-ios
+//
+//  Created by Harry on 09.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
